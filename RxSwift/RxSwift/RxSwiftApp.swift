d8eb4c3ebf4501a455f0ad79e71bfebd0c24a918
ios-carki <94.nogari@gmail.com>
2024-08-20 09:07:38 +0900

    [init] Initial Commit

---
+++ RxSwift/RxSwift/RxSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftApp.swift
+//  RxSwift
+//
+//  Created by OWEN on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
